d9fc9baff7d4ab5365b7aa40d0516d63e3bf78b0
harshh-debug <harshj9705@gmail.com>
2026-01-23 18:13:28 +0530

    deployment changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,17 +8,19 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-console.log(process.env.FRONTEND_URL)
-
+const PORT = process.env.PORT || 3000;
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL||"http://localhost:5173",
+		origin: process.env.FRONTEND_URL || "http://localhost:5173",
 		credentials: true,
-	})
+	}),
 );
 
+app.get("/ping", (req, res) => {
+	res.status(200).json({ message: "Server is awake" });
+});
 app.use("/api/v1", mainRouter);
 
-app.listen(3000, () => {
-	console.log("Listening at 3000");
+app.listen(PORT, () => {
+	console.log(`Listening at ${PORT}`);
 });
